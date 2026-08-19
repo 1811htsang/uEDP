997682ca14fcc66c01a0bf87e4a77195d33dc2c3
Huỳnh Thanh Sang <97782010+1811htsang@users.noreply.github.com>
2026-08-19 05:53:07 +0000

    feat(lstaxer.vlid)[sang]: add PRE with PAC step for validation strategy

--- pltf/testspec/lstaxer/vlid_lstaxer.py
+++ pltf/testspec/lstaxer/vlid_lstaxer.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel, field_validator
 from typing import List, Optional, Dict
 
-# NOTE - Check strategy
+# NOTE - Validation Strategy for lstaxer.vlid 
 """
 1. Dangling Alias Check
 2. Context-Type Match
-3. UST cross reference
+3. UST cross Reference
+4. Post Resource Existance
+5. Policy Alignment Check
 """
