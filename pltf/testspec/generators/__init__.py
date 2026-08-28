b9fc404db3a068cd5ef1184da6674983c2edfff9
Huỳnh Thanh Sang <97782010+1811htsang@users.noreply.github.com>
2026-08-28 05:59:13 +0000

    feat(generators)[sang]: temporal remove module inclusion & export for appc_tsgen

--- pltf/testspec/generators/__init__.py
+++ pltf/testspec/generators/__init__.py
@@ -1,5 +1,5 @@
 # NOTE - Export modules for use in other packages
 
-from . import appcfg_tsgen, corecfg_tsgen, palcfg_tsgen, appdecl_tsgen, arch_dir_tsgen, arch_h_tsgen, arch_c_tsgen, cfpcall_tsgen, appc_tsgen
+from . import appcfg_tsgen, corecfg_tsgen, palcfg_tsgen, appdecl_tsgen, arch_dir_tsgen, arch_h_tsgen, arch_c_tsgen, cfpcall_tsgen
 
 # NOTE - Export functions for use in other packages
