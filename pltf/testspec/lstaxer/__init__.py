def5b39b4a0b5861640051db606218863f84adc8
1811htsang <macsin233@gmail.com>
2026-08-31 22:27:53 +0700

    feat(lukupmodel)[sang]: add sample handler for event parsing

--- pltf/testspec/lstaxer/__init__.py
+++ pltf/testspec/lstaxer/__init__.py
@@ -2,5 +2,7 @@
 
 from . import kre8_lstaxer, strucjec_lstaxer, symresolv_lstaxer
 
+# TASK - Add task to export vlid after finish lukupmodel
+
 # NOTE - Export functions for use in other packages
 
