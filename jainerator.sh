b271e2e42ad029140723678b8c68164860d03d84
Huỳnh Thanh Sang <97782010+1811htsang@users.noreply.github.com>
2026-09-04 10:24:50 +0000

    feat(jainerator, fpregen): update jainerator.sh and fpregen.py to enhance logging messages for better traceability

--- jainerator.sh
+++ jainerator.sh
@@ -4,3 +4,4 @@ echo "[ENTRY] call pycdscriptor.jnerator.postgen.cgen"
 python -m pltf.pycdscriptor.jnerator.postgen.cgen \
   --yaml sources/app/lstaxizer.yaml \
   --output sources/app/app.c
+echo "[INFO] jainerator has done"
