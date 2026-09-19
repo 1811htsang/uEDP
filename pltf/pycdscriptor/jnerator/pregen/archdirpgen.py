67145bd04646c26ad3826697cfb710488c598a18
1811htsang <macsin233@gmail.com>
2026-09-19 16:44:41 +0700

    feat(arch)[sang]: update directory and file creation with adding perservation for old build

--- pltf/pycdscriptor/jnerator/pregen/archdirpgen.py
+++ pltf/pycdscriptor/jnerator/pregen/archdirpgen.py
@@ -5,7 +5,16 @@ def main(context):
   arch_dir = "sources/pal/arch"
   # Create folder for arch
   try:
-    os.makedirs(arch_dir + f"/{context['arch_name']}", exist_ok=True)
+    # NOTE - Add checking if the directory is already exists, if not, create it. If it exists, check if it's empty or not. If it's not empty, print a warning message.
+    if os.path.exists(arch_dir + f"/{context['arch_name']}"):
+      if os.listdir(arch_dir + f"/{context['arch_name']}"):
+        print(f"[WARNING] Directory {arch_dir}/{context['arch_name']} is not empty. Reject creation.")
+        return
+      else:
+        print(f"[INFO] Directory {arch_dir}/{context['arch_name']} already exists and is empty. Proceeding with creation.")
+    else:
+      print(f"[INFO] Directory {arch_dir}/{context['arch_name']} does not exist. Creating it.")
+      os.makedirs(arch_dir + f"/{context['arch_name']}", exist_ok=True)
   except PermissionError:
     print(f"[ERROR] Permission denied: Cannot create directory")
   except OSError as e:
